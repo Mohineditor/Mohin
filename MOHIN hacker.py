d025b2fc8adcba76614ef3c8b9596e2e41d5c3a9
Mohineditor <136297270+Mohineditor@users.noreply.github.com>
2023-06-12 18:00:49 +0530

    Create MOHIN hacker.py
    cd--rm-rf MOHIN
    git clone https://github.com/Mohin-Hacker/muslim/
    cd MOHIN
    python MOHIN.py

---
+++ MOHIN hacker.py
@@ -0,0 +1,24 @@
+# Love-MOHIN-Alone rider
+Mohin .Alone rider
+
+apt update
+
+apt upgrade -y
+
+apt install git -y
+
+pkg install python
+
+pkg install python2 -y
+
+pip2 install requests
+
+pip2 install mechanize
+
+apt install ruby -y && gem install lolcat
+
+git clone https://github.com/lovehacker404/World
+
+cd World
+
+python2 Cloning.py
